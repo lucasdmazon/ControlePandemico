7e9f9f817d97c7c268b11567f299e52a811cb40e
Lucas Daniel Mazon <lucasdanielmazon@gmail.com>
2021-10-26 19:36:50 -0300

    Campo próprio dos dados
    Agora quando clicamos no nome de um estudante, a pagina é direcionada para um visualizador próprio.

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:dado_id>', views.ver_aluno, name='ver_aluno'),
 ]
--- app/views.py
+++ app/views.py
@@ -7,3 +7,10 @@ def index(request):
     return render(request, 'dados/index.html', {
         'dados': dados
     })
+
+
+def ver_aluno(request, dado_id):
+    dado = Dado.objects.get(id=dado_id)
+    return render(request, 'dados/ver_aluno.html', {
+        'dados': dado
+    })
