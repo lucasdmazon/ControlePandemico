a43971bb6ff293630b5f9179f2fe8c24b136ee9a
Lucas Daniel Mazon <lucasdanielmazon@gmail.com>
2021-10-26 18:42:26 -0300

    SuperAdmin
    Criação da conta mestre da secretaria.

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Categoria, Dado
 
-# Register your models here.
+
+class DadoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'numero', 'serie', 'data_criacao', 'data_nascimento', 'categoria')
+    list_display_links = ('id', 'nome', 'sobrenome')
+    list_filter = ('nome', 'sobrenome')
+    list_per_page = 10
+    search_fields = ('nome', 'sobrenome', 'numero', 'serie', 'categoria')
+
+
+admin.site.register(Categoria)
+admin.site.register(Dado, DadoAdmin)
+
--- app/models.py
+++ app/models.py
@@ -5,6 +5,9 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nome
+
 
 class Dado(models.Model):
     nome = models.CharField(max_length=255)
@@ -13,8 +16,9 @@ class Dado(models.Model):
     email = models.CharField(max_length=255, blank=True)
     data_criacao = models.DateTimeField(default=timezone.now)
     numero = models.IntegerField()
-    serie = models.CharField(max_length=100)
+    serie = models.CharField(max_length=20)
     data_nascimento = models.DateField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
 
-
+    def __str__(self):
+        return self.nome
