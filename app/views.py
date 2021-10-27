76d79c1d3b964b6ba7ccb2042cad798d444cde78
Lucas Daniel Mazon <lucasdanielmazon@gmail.com>
2021-10-26 21:53:49 -0300

    Paginator
    Agora a tabela possui um paginator para organizar melhor o conteúdo.

--- app/views.py
+++ app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Dado
+from django.core.paginator import Paginator
 
 
 def index(request):
     dados = Dado.objects.all()
+    paginator = Paginator(dados, 10)
+    page = request.GET.get('page')
+    dados = paginator.get_page(page)
     return render(request, 'dados/index.html', {
         'dados': dados
     })
