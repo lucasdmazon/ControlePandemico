85e1e055a33fc535f379af87695efb926ea09907
Lucas Daniel Mazon <lucasdanielmazon@gmail.com>
2021-10-26 22:10:49 -0300

    Organização da tabela
    Agora a tabela está sendo ordenada pelo nome, e foi concertado o bug de acesso pela url a dados marcados como não mostrar.

--- app/views.py
+++ app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Dado
 from django.core.paginator import Paginator
 
 
 def index(request):
-    dados = Dado.objects.all()
+    dados = Dado.objects.order_by('-nome').filter(
+        mostrar = True
+    )
     paginator = Paginator(dados, 10)
     page = request.GET.get('page')
     dados = paginator.get_page(page)
@@ -15,6 +18,10 @@ def index(request):
 
 def ver_aluno(request, dado_id):
     dado = get_object_or_404(Dado, id=dado_id)
+
+    if not dado.mostrar:
+        raise Http404()
+
     return render(request, 'dados/ver_aluno.html', {
         'dados': dado
     })
