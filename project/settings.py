64aee5d1b1c7a7e5b7f52a3c49d6d2cf61fc8b68
Lucas Daniel Mazon <lucasdanielmazon@gmail.com>
2021-11-01 22:20:40 -0300

    Primeiro envio

--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,3 +142,5 @@ MESSAGE_TAGS = {
     constants.SUCCESS: 'alert-success',
     constants.INFO: 'alert-info',
 }
+
+django_heroku.settings(locals())
