e8c79efa163f0591c87e9e924c7ec3e441098aca
Lucas Daniel Mazon <lucasdanielmazon@gmail.com>
2021-11-01 23:27:32 -0300

    Adicionar arquivos para deploy com Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn project.wsgi
+release: python3 manage.py migrate
+web: gunicorn project.wsgi --preload --log-file –
--- project/settings/base.py
+++ project/settings/base.py
@@ -15,7 +15,7 @@ import os
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,4 +144,5 @@ MESSAGE_TAGS = {
     constants.INFO: 'alert-info',
 }
 
-django_heroku.settings(locals())
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
